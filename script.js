00d70d6740097c3c0145f470c2987a13eeccb032
qvarkk <xumdes@gmail.com>
2022-07-24 15:03:26 +0500

    many tiring things that i don't want to list

--- script.js
+++ script.js
@@ -1,17 +1,37 @@
-const DEFAULT_VALUE = '0';
-
-let currentValue = DEFAULT_VALUE;
+let currentValue = '0';
 let currentCalc = 0;
 
-const screenPara = document.getElementById('calculations');
+let screenCalc = document.getElementById('calculations');
+let screenRecent = document.getElementById('recent');
 
-function addNewValue() {
-    
+function addNewValue(value) {
+    checkAction('adding value');
+    if (screenCalc.innerHTML !== '0') {
+        screenCalc.innerHTML += value;
+    } else {
+        screenCalc.innerHTML = value;
+    }
+}
+
+function addNewAction(operator) {
+    if (checkAction('operating')) {
+        operate(operator);
+    } else {
+        screenRecent.innerHTML = currentCalc + operator;
+    }
+}
+
+function clearScreen() {
+    screenCalc.innerHTML = '0';
+    screenRecent.innerHTML = '0';
+    currentCalc = 0;
+    lastAction = '';
+    lastOperator = '';
 }
 
 //removing
-const eraseButton = document.getElementById('erase');
 const clearButton = document.getElementById('clear');
+const eraseButton = document.getElementById('erase');
 //operators
 const additionButton = document.getElementById('addition');
 const subtractionButton = document.getElementById('subtraction');
@@ -33,8 +53,8 @@ const decimalButton = document.getElementById('decimal');
 const equalsButton = document.getElementById('equals');
 
 //removing
+clearButton.onclick = () => {clearScreen()};
 eraseButton.onclick = () => {removeOldValue()};
-clearButton.onclick = () => {clearOldValues()};
 //operators
 additionButton.onclick = () => {addNewAction('+')};
 subtractionButton.onclick = () => {addNewAction('-')};
@@ -53,4 +73,45 @@ nineButton.onclick = () => {addNewValue('9')};
 zeroButton.onclick = () => {addNewValue('0')};
 //special
 decimalButton.onclick = () => {addDecimalPoint()};
-equalsButton.onclick = () => {solveEquasion()};
+equalsButton.onclick = () => {addNewAction('=')};
+
+let lastAction = 'operating';
+function checkAction(action) {
+    if (lastAction === 'operating') {
+        lastAction = action;
+        return false;
+    } else {
+        lastAction = action;
+        return true;
+    }
+}
+
+let lastOperator = '';
+function doAction(a, b) {
+    if (lastOperator === '+') {
+        currentCalc = b + a;
+    } else if (lastOperator === '-') {
+        currentCalc = b - a;
+    } else if (lastOperator === '*') {
+        currentCalc = b * a;
+    } else if (lastOperator === '/') { 
+        currentCalc = b / a;
+    } else {
+        currentCalc = screenCalc.innerHTML;
+    }
+}
+
+function operate(operator) {
+    let a = parseFloat(screenCalc.innerHTML);
+    let b = parseFloat(screenRecent.innerHTML.slice(0,-1));
+    doAction(a, b);
+    if (operator !== '=') {
+        screenRecent.innerHTML = currentCalc + operator;
+        lastOperator = operator;
+        screenCalc.innerHTML = '0';
+    } else {
+        screenRecent.innerHTML = b + lastOperator + a + '='
+        lastOperator = '='
+        screenCalc.innerHTML = currentCalc
+    }
+}
